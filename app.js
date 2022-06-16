2200b6672e8a1853ab39945202b757b11fd64396
Thibault Bardinet-Langlois <langlois.bardinet.thibault@gmail.com>
2022-06-16 23:14:48 +0200

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // app.js
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
+const apiRoutes = require('./routes')
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const app = express()
+app.use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(apiRoutes)
 
-module.exports = app;
+module.exports = app
--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
 });
 
 connection.connect(function(err) {
